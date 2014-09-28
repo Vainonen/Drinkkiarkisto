1bc032d24533f8a98d4e6d1ab27b256c0fc11c6a
Vainonen <okko.vainonen@cs.helsinki.fi>
2014-09-28 22:58:21 +0300

    sql-tiedostoja muokattu

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -9,7 +9,7 @@ adminoikeus boolean
 
 CREATE TABLE drinkit
 (
-drinkki_id SERIAL PRIMARY KEY NOT NULL, 
+id SERIAL PRIMARY KEY NOT NULL, 
 nimi varchar(255),
 aliakset varchar(255),
 drinkkityyppi varchar(255),
@@ -18,20 +18,20 @@ valmistustapa varchar(255)
 
 CREATE TABLE raakaaineet
 (
-aine_id SERIAL PRIMARY KEY NOT NULL, 
+id SERIAL PRIMARY KEY NOT NULL, 
 nimi varchar(255)
 );
 
 CREATE TABLE muokkaajat
 (
 id integer REFERENCES kayttajat (id),
-drinkki_id integer REFERENCES drinkit (drinkki_id),
+drinkki_id integer REFERENCES drinkit (id),
 aika timestamp
 );
 
 CREATE TABLE ainesosat
 (
-tilavuus int,
-drinkki_id integer REFERENCES drinkit (drinkki_id),
-aine_id integer REFERENCES raakaaineet (aine_id)
+tilavuus integer,
+drinkki_id integer REFERENCES drinkit (id),
+aine_id integer REFERENCES raakaaineet (id)
 );
